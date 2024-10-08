99b7b3bb992c755122d55b0fcb5b3e9f1ae2bb5c
KimJY37 <gmlwlswldbs@naver.com>
2024-10-08 15:21:39 +0900

    Add: 목적지 정보 업데이트 로직 추가

--- location/src/main/java/alpcaproject/domain/LocRegistered.java
+++ location/src/main/java/alpcaproject/domain/LocRegistered.java
@@ -13,8 +13,17 @@ public class LocRegistered extends AbstractEvent {
 
     private String locId;
     private String customerId;
+<<<<<<< HEAD
     private Double locX;
     private Double locY;
+=======
+    private String familyId;
+    private String loc1;
+    private String loc2;
+    private String loc3;
+    private String loc4;
+    private String loc5;
+>>>>>>> 536b31b (commit)
 
     public LocRegistered(Location aggregate) {
         super(aggregate);
