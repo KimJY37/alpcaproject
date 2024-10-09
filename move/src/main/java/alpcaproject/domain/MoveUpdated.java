8f9ef12952028d441396ffc24dfda70423cf0859
KimJY37 <82562000+KimJY37@users.noreply.github.com>
2024-10-09 15:53:24 +0900

    commit

--- move/src/main/java/alpcaproject/domain/MoveUpdated.java
+++ move/src/main/java/alpcaproject/domain/MoveUpdated.java
@@ -11,7 +11,19 @@ import lombok.*;
 @ToString
 public class MoveUpdated extends AbstractEvent {
 
-    private Long id;
+    private String moveId;
+    private String familyId;
+    private String role;
+    private String startRdnAddr;
+    private Double startLocX;
+    private Double startLocY;
+    private Double goalLocX;
+    private Double goalLocY;
+    private Integer distance;
+    private Integer duration;
+    private String path;
+    private String status;
+    private String customerId;
 
     public MoveUpdated(Move aggregate) {
         super(aggregate);
